11262fea464701768ebd6cb9ced2f4c518611fa4
Nariman <aliyevnariman98@gmail.com>
2025-10-14 22:00:39 +0200

    Image optimization update for the CustomScrolling file

--- src/app/(site)/providers.tsx
+++ src/app/(site)/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { ReactNode } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const isServer = typeof window === "undefined";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backdropFilter: {
-        'none': 'none',
-        'blur': 'blur(20px)',
+        none: "none",
+        blur: "blur(20px)",
       },
 
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
 
-      backgroundColor: theme => ({
-        'transparent-white': 'rgba(255, 255, 255, 0.01)',
-     }),
-
-     
+      backgroundColor: (theme) => ({
+        "transparent-white": "rgba(255, 255, 255, 0.01)",
+      }),
     },
 
     fontFamily: {
-     openSans: ["Open Sans", "sans-serif"]
-    }
-    
+      openSans: ["Open Sans", "sans-serif"],
+    },
   },
   variants: {
     extend: {
-      backdropFilter: ['responsive'],
+      backdropFilter: ["responsive"],
     },
   },
-  plugins: [
-    require('@tailwindcss/postcss7-compat')
-  ],
-}
+};
