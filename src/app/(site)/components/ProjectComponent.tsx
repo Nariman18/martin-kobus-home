b1b38870c502a811631fa15fee68fd176e878139
Nariman <aliyevnariman98@gmail.com>
2025-12-22 19:43:51 +0100

    fixes made

--- src/app/(site)/components/ProjectComponent.tsx
+++ src/app/(site)/components/ProjectComponent.tsx
@@ -1,9 +1,11 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import { Project } from "../../../../types/Project";
 import { getProjects } from "../../../../sanity/sanity-utils";
 import Link from "next/link";
 import Image from "next/image";
+import { urlFor } from "../../../../sanity/lib/image";
 
 function ProjectComponent() {
   const [projects, setProjects] = useState<Project[]>([]);
@@ -12,7 +14,7 @@ function ProjectComponent() {
     const fetchData = async () => {
       const pagesData = await getProjects();
       const sortedPagesData = pagesData.sort(
-        (b, a) =>
+        (a, b) =>
           new Date(b._createdAt).getTime() - new Date(a._createdAt).getTime()
       );
       setProjects(sortedPagesData);
@@ -23,15 +25,19 @@ function ProjectComponent() {
 
   return (
     <div className="flex items-center justify-center mt-[100px] pb-5">
-      <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 min-[1537px]:gap-10 gap-5">
+      <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
         {projects.map((project) => (
           <Link key={project._id} href={`projects/${project.slug}`}>
-            {project.image && (
+            {project.coverImage?.asset && (
               <div className="relative w-[320px] h-[400px]">
                 <Image
-                  src={project.image}
+                  src={urlFor(project.coverImage.asset)
+                    .width(800)
+                    .quality(80)
+                    .url()}
                   alt={project.name}
-                  fill={true}
+                  fill
+                  sizes="320px"
                   className="object-cover"
                 />
               </div>
