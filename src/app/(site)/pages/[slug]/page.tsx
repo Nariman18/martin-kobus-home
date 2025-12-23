65693b9c094cbede5f9c8a7a123ea009576ae20f
Nariman <aliyevnariman98@gmail.com>
2025-12-23 20:33:16 +0100

    added also the force no store variable

--- src/app/(site)/pages/[slug]/page.tsx
+++ src/app/(site)/pages/[slug]/page.tsx
@@ -1,4 +1,5 @@
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import { getPage } from "../../../../../sanity/sanity-utils";
 import CustomScrolling from "../../components/CustomScrolling";
