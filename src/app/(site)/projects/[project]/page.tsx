f96c6837f2e9b48f96b0b068fca3eac005ad2761
Nariman <aliyevnariman98@gmail.com>
2025-12-23 20:42:26 +0100

    added new force-dynamic and force-no-cache to the project page now

--- src/app/(site)/projects/[project]/page.tsx
+++ src/app/(site)/projects/[project]/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { getProject } from "../../../../../sanity/sanity-utils";
 import CustomScrolling from "../../components/CustomScrolling";
 
